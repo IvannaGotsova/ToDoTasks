9372b1251bb54c492e76c431a91df8ccf2155ea2
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-18 14:22:06 +0300

    Added All buttons

---
+++ js/main.js
@@ -0,0 +1,70 @@
+import Item from "./item";
+import Tasks from "./tasks";
+import Notes from "./notes";
+import Lists from "./lists";
+
+
+const tasks = new Tasks();
+const notes = new Notes();
+const lists = new Lists();
+
+
+const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
+const getParams = match => {
+    const values = match.result.slice(1);
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+    return Object.fromEntries(keys.map((key, i) => {
+        return [key, values[i]];
+    }));
+};
+
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+};
+
+const router = async () => {
+    const routes = [
+        { path: "/", view: index },
+        { path: "/tasks", view: tasks },
+        { path: "/notes", view: notes },
+        { path: "/notes/:id", view: note },
+        { path: "/lists", view: lists },
+        { path: "/lists/:id", view: list }
+    ];
+
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            result: location.pathname.match(pathToRegex(route.path))
+        };
+    });
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
+
+    if (!match) {
+        match = {
+            route: routes[0],
+            result: [location.pathname]
+        };
+    }
+
+    const view = new match.route.view(getParams(match));
+
+    document.querySelector("#app").innerHTML = await view.getHtml();
+};
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+
+    router();
+});

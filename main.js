abd7d92f651dc4c9cbacaa388d66a2ed2dcfdf88
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-18 14:14:06 +0300

    Removed

--- main.js
+++
@@ -1,9 +0,0 @@
-import Item from "./item";
-import Tasks from "./tasks";
-import Notes from "./notes";
-import Lists from "./lists";
-
-
-const tasks = new Tasks();
-const notes = new Notes();
-const lists = new Lists();

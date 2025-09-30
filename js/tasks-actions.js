cc61f8d53602d987a5e90108a98d9d425cd518d4
IvannaGotsova <ivannagotsova@outlook.com>
2025-09-30 20:51:17 +0300

    Added done functionality

--- js/tasks-actions.js
+++ js/tasks-actions.js
@@ -105,4 +105,15 @@
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }
   
-  
+function doneTask(task) {
+    task.completed = true;
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks = tasks.map(t => t.id === task.id ? { ...t, completed: true } : t);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    localStorage.setItem(`task-${task.id}-done`, "true");
+  }
+
+
+
+
+
